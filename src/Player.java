2f2c645703bf98e5c63b5dddcf4692a2a4212a9f
BT_1E10_06 <dliu3541@bths.edu>
2022-03-14 13:12:55 -0400

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,31 @@
+public class Player
+{
+    String name;
+    int guessNumber;
+
+    public Player(String name)
+    {
+        this.name = name;
+        guessNumber = 0;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+
+    public int getGuessNumber()
+    {
+        return guessNumber;
+    }
+
+    public void setGuessNumber(String newGuessNumber)
+    {
+        name = newGuessNumber;
+    }
+}
